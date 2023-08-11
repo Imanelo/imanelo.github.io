8dae645582ff87fe88f7de4a6409ff4c37b63ed2
imanel257 <elouadiimane94@gmail.com>
2023-08-11 11:21:38 +0100

    about  me section edited

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,9 +1,9 @@
 export const aboutData = {
-    title: "Bio",
-    description1: "Imane El Ouadi is a Telecom - Networks engineer with a passion for becoming a NetDevOps engineer. With expertise in networking, security, and programming, she aims to automate network operations and infrastructure. Imane holds a Master's degree from Telecom Saint-etienne in France.",
-    description2: "Her experience in a startup specializing in Wi-Fi and geolocation solutions for buses led to a significant increase in clients within her first year. Adaptable and skilled in communication, she values cultural diversity gained from her academic and professional journey.",
-    description3: "Beyond her professional pursuits, Imane enjoys drawing, painting, and is a nature lover and sports enthusiast. Her discipline led her to win the national Taekwondo championship, showcasing her commitment to excellence.",
-    description4: "Imane is currently sharpening her programming and automation skills while gaining expertise in cloud and containerization technologies. Actively participating in the ALX training for AWS cloud program, she stays current with Cloud computing trends.",
+    title: "About Me",
+    description1: "Hello Beautiful people ! I am Imane El Ouadi, a Telecom - Networks engineer with a passion for becoming a NetDevOps engineer. With my expertise in networking, security, and programming, I aim to automate network operations and infrastructure. I hold a Master's degree from Telecom Saint-etienne in France.",
+    description2: "My experience in a startup specializing in Wi-Fi and geolocation solutions for buses led to a significant increase in clients within my first year. I am adaptable and skilled in communication, valuing the cultural diversity I've gained from my academic and professional journey.",
+    description3: "Beyond my professional pursuits, I enjoy drawing, painting, and I am a nature lover and sports enthusiast. My discipline led me to win the national Taekwondo championship, showcasing my commitment to excellence.",
+    description4: "Currently, I am sharpening my programming and automation skills while gaining expertise in cloud and containerization technologies. I am actively participating in the ALX training for AWS cloud program, staying current with Cloud computing trends.",
     description5: "Seeking to join a value-driven company that embraces DevOps and automation practices,she invites connections to explore potential collaborations and appreciates the consideration for future opportunities.",
     image: 1
 }
