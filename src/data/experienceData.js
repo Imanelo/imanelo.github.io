760dda347c709434e0a2aa638f677ec3eed52f26
imanelo <ntaloooz@gmail.com>
2024-12-04 22:07:40 +0100

    add recent experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,20 +2,27 @@ import  quote  from '../assets/png/quote10.png'
 export const experienceData = [
     {
         id: 1,
+        company: 'Teleperformance',
+        jobtitle: 'Microsoft Support Engineer : Microsoft Exchange' ,
+        startYear: 'May 2024',
+        endYear: 'Nov 2023'
+    },
+    {
+        id: 2,
         company: 'SQLI',
         jobtitle: 'Rest Api Test automaton intern',
         startYear: 'Jun 2023',
         endYear: 'Nov 2023'
     },
     {
-        id: 2,
+        id: 3,
         company: 'Berthiez',
         jobtitle: 'Industrial Project : Feasibility study of a vision system',
         startYear: 'May 2016',
         endYear: 'Sep 2016'
     },
     {
-        id: 3,
+        id: 4,
         company: 'ONCF',
         jobtitle: 'Internship: Cellular planning of GSM-R railway network',
         startYear: 'Apr 2015',
